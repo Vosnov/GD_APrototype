6a8ece2a1dbd15077841f45b16c4ebde7478d724
Vitaliy Stepanov <vitaly.stepanov@itl.team>
2023-07-05 13:51:07 +0300

    door accept node

--- Scripts/Helpers/active_node.gd
+++ Scripts/Helpers/active_node.gd
@@ -3,28 +3,27 @@ class_name ActiveNode
 
 signal action_pressed
 
-@export var SPRITE: Sprite3D
+@export var SPRITE: PackedScene
 @export var AREA: Area3D
 
 var player_in_zone = false
+var sprite: Node
 
 func _ready():
 	AREA.connect('body_entered', _on_area_3d_body_entered)
 	AREA.connect('body_exited', _on_area_3d_body_exited)
-	SPRITE.visible = false
 
 func _on_area_3d_body_entered(_body):
 	player_in_zone = true
-	SPRITE.visible = true
+	if SPRITE:
+		sprite = SPRITE.instantiate()
+		add_child(sprite)
 
 func _on_area_3d_body_exited(_body):
 	player_in_zone = false
-	SPRITE.visible = false
+	if sprite: sprite.queue_free()
 
 func _input(_event):
 	if !player_in_zone: return
 	if Input.is_action_just_pressed("action"):
 		action_pressed.emit()
-
-func change_sprite(sprite: Sprite3D):
-	SPRITE = sprite
---
+++ Scripts/Helpers/door_active_node.gd
@@ -0,0 +1,32 @@
+extends ActiveNode
+class_name DoorActiveNode
+
+enum AcceptState {
+	ACCEPT,
+	NEED_ACCEPT,
+	CLOSE
+}
+
+@export var ACCEPT_SPRITE: PackedScene
+@export var NEED_ACCEPT_SPRITE: PackedScene
+@export var CLOSE_SPRITE: PackedScene
+
+@export var ACCEPT_STATE = AcceptState.ACCEPT:
+	set(value):
+		SPRITE = get_sprite_by_state(value)
+		ACCEPT_STATE = value
+
+func get_sprite_by_state(state: AcceptState):
+	if state == AcceptState.ACCEPT:
+		return ACCEPT_SPRITE
+	if state == AcceptState.NEED_ACCEPT:
+		return NEED_ACCEPT_SPRITE
+	if state == AcceptState.CLOSE:
+		return CLOSE_SPRITE
+
+func change_sprite(state: AcceptState):
+	ACCEPT_STATE = state
+	if sprite != null: 
+		sprite.queue_free()
+		sprite = get_sprite_by_state(state).instantiate()
+		add_child(sprite)
