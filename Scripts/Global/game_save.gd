5abe2c95230542271c53a040874a5d87c0d6f3b6
Vitaliy Stepanov <vitaly.stepanov@itl.team>
2023-07-13 01:52:07 +0300

    save room and save ui

--- Scripts/Global/game_save.gd
+++ Scripts/Global/game_save.gd
@@ -1,28 +1,25 @@
 extends Node
 
-@export var SAVE_FILE_NAME = "user://save.dat"
+@export var PATH = 'user://'
+@export var SAVE_FILE_NAME = "save"
+@export var EXTENSION = '.dat'
 
-func save():
-	var file = FileAccess.open(SAVE_FILE_NAME, FileAccess.WRITE)
+func save(index: int):
+	var file = FileAccess.open(get_file_name(index), FileAccess.WRITE)
 	if file == null:
 		printerr("Save file error")
 		return
 		
 	var data = {
 		'slots': Inventory.SLOTS.map(func(slot): return slot.get_data()),
-		'global': GlobalVariables.get_data()
+		'global': GlobalVariables.get_data(),
+		'save_data': GlobalVariables.get_save_data()
 	}
 	file.store_string(var_to_str(data))
 
-func load_save():
-	if not FileAccess.file_exists(SAVE_FILE_NAME): 
-		printerr('Load file not found')
-		return
-		
-	var file = FileAccess.open(SAVE_FILE_NAME, FileAccess.READ)
-	if file == null:
-		printerr("Load file error")
-		return
+func load_save(index: int):
+	var file = load_file(index)
+	if file == null: return
 	
 	var data = str_to_var(file.get_as_text())
 
@@ -30,6 +27,26 @@ func load_save():
 	GlobalVariables.set_data(data.global)
 	
 	Events.inventory_update.emit()
+
+func load_file(index: int) -> FileAccess:
+	if not FileAccess.file_exists(get_file_name(index)): 
+		printerr('Save file not found')
+		return null
+		
+	var file = FileAccess.open(get_file_name(index), FileAccess.READ)
+	if file == null:
+		printerr("Cant read save file")
+		return null
+	return file
+
+func get_save_data(index: int):
+	var file = load_file(index)
+	if file == null: return null
+	var data = str_to_var(file.get_as_text())
+	return data.save_data
+
+func get_file_name(index: int) -> String:
+	return str(PATH, SAVE_FILE_NAME, '_', index, EXTENSION)
 
 func parse_slots(slots_dict: Array) -> Array[SlotData]:
 	var new_slots: Array[SlotData] = []
--- Scripts/Global/global_variables.gd
+++ Scripts/Global/global_variables.gd
@@ -7,9 +7,11 @@ var player_spawn_data: Dictionary = {}
 var player_ammo_load: Dictionary = {}
 var active_gun: GunData
 
+var menu_ui_is_open = false
 var player_is_runing = false
 var player_is_aim = false
 var player_target_is_full = false
+var save_data: Dictionary = {}
 
 func get_data() -> Dictionary:
 	return {
@@ -24,3 +26,12 @@ func set_data(dict: Dictionary):
 	no_spawn_enemys = dict.no_spawn_enemys
 	player_spawn_data = dict.player_spawn_data
 	player_ammo_load = dict.player_ammo_load
+
+func get_save_data() -> Dictionary:
+	save_data.date = get_current_date()
+	return save_data
+
+func get_current_date() -> String:
+	var date = Time.get_date_dict_from_system()
+	var time = Time.get_time_dict_from_system()
+	return "%02d.%02d.%02d %02d:%02d" % [date.day, date.month, date.year, time.hour, time.minute]
