b44bee77b1dc3b3a299a393121c785ddd40b994d
Vitaliy Stepanov <vitaly.stepanov@itl.team>
2023-07-20 12:15:40 +0300

    global ui stream

--- Scripts/Global/global_sound.gd
+++ Scripts/Global/global_sound.gd
@@ -1,11 +1,10 @@
 extends Node
 
-@export var UI_STREAM: PackedScene
-
 func play_ui(stream: AudioStream):
-	var stream_player = UI_STREAM.instantiate()
-	add_child(stream_player)
+	var stream_player = AudioStreamPlayer.new()
+	stream_player.bus = 'UI'
 	stream_player.stream = stream
+	add_child(stream_player)
 	stream_player.play()
 	await stream_player.finished
 	stream_player.queue_free()
