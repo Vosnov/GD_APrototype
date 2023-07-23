8f6a3177996b7f6e6dba2d5660ccf3230c5fa70b
Vitaliy Stepanov <vitaly.stepanov@itl.team>
2023-07-23 23:32:07 +0300

    pause ui

--- Scripts/Global/scene_transition.gd
+++ Scripts/Global/scene_transition.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
 
 @onready var animation_player = $AnimationPlayer
+var scene_changing = false
 
 func change_scene(path: String):
+	if scene_changing: return
+	
+	scene_changing = true
 	animation_player.play("scene_trans")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file(path)
 	#get_tree().change_scene_to_packed(scene)
 	animation_player.play_backwards("scene_trans")
+	scene_changing = false
