ac2f9fef6419ced994e0fc4fe095ea00cd7747ca
Vitaliy Stepanov <vitaly.stepanov@itl.team>
2023-07-30 01:33:02 +0300

    locale and control map

--- Scripts/Global/events.gd
+++ Scripts/Global/events.gd
@@ -16,6 +16,7 @@ signal player_hp_ui(hp: float)
 signal player_reload_data_ui(amount: int, total_amount: int)
 signal message_ui(message: String)
 signal message_close_ui(message: String)
+signal map_set_move_mode(value: bool)
 #INVENTORY
 signal item_pick_up(slot_data: SlotData, body: Node3D)
 signal inventory_remove_item(item: ItemData)
